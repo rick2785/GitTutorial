9947e8da80eafb1651d6baf871a028f3bb942a72
Rickey Hrabowskie <rickeyhrabowskie@rickeys-macbook-pro.local>
2016-07-22 16:16:43 -0500

    Added MapViewController

--- GitXcode/ViewController.swift
+++ GitXcode/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Hello World!")
+        let mapViewController = MapViewController()
+        print(MapViewController)
     }
 
     override func didReceiveMemoryWarning() {
